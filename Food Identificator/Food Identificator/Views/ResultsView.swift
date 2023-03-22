8b5f36d0b4e8f6b1ebe380855d365edf8ea44fa9
Lawrence Luo <xllluo08@gmail.com>
2023-03-22 04:23:33 -0400

    added model and extensions file + code for running the image through the model

--- Food Identificator/Food Identificator/Views/ResultsView.swift
+++ Food Identificator/Food Identificator/Views/ResultsView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreML
+import UIKit
 
 struct ResultsView: View {
     var body: some View {
@@ -22,5 +24,69 @@ struct ResultsView: View {
 struct ResultsView_Previews: PreviewProvider {
     static var previews: some View {
         ResultsView()
+    }
+}
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    private let imageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(systemName: "photo")
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }()
+    
+    private let label: UILabel = {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.text = "Select Image"
+            label.numberOfLines = 0
+            return label
+        }()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.addSubview(label)
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width-40,
+            height: view.frame.size.width-40)
+        label.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top+(view.frame.size.width-40)+10,
+            width: view.frame.size.width-40,
+            height: 100
+        )
+    }
+
+    
+    private func analyzeImage(image: UIImage?)
+    {
+        guard let buffer = image?.resize(size: CGSize(width: 224, height: 224))?
+                .getCVPixelBuffer() else {
+            return
+        }
+
+        do {
+            let config = MLModelConfiguration()
+            let model = try SmallTestSet(configuration: config)
+            let input = SmallTestSetInput(image: buffer)
+            // this error might be due to the config of the model being different
+            let output = try model.prediction(input: input)
+            let text = output.sceneLabel
+            // another error, need to figure out the correct output parameters of the model
+            label.text = text
+        }
+        catch {
+            print(error.localizedDescription)
+        }
     }
 }
